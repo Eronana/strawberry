35361eb276af6f78bc997964bc1ed35f533ac69a
Erona <erona@loli.bz>
2017-05-03 10:06:24 +0800

    add stack

---
+++ include/stack.hpp
@@ -0,0 +1,53 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <vector>
+template<typename T>
+class Stack
+{
+private:
+    std::vector<T> data;
+public:
+    T &top(int n=0)
+    {
+        return *(data.rbegin()+n);
+    }
+
+    void add(int n)
+    {
+        data.resize(data.size()+n);
+    }
+
+    void sub(int n)
+    {
+        data.resize(data.size()-n);
+    }
+
+    T pop()
+    {
+        T ret=top();
+        sub(1);
+        return ret;
+    }
+
+    void push(const T &a)
+    {
+        data.push_back(a);
+    }
+
+    void clear()
+    {
+        data.clear();
+    }
+
+    size_t size()
+    {
+        return data.size();
+    }
+
+    T &operator[](int idx)
+    {
+        return data[idx];
+    }
+};
+#endif
