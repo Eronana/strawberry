74137dc40dc9e6155833241fd4e07787825e2596
Erona <erona@loli.bz>
2017-05-08 21:11:34 +0800

    fix errors in g++

--- src/ast_method.cpp
+++ src/ast_method.cpp
@@ -1,5 +1,6 @@
 #include "ast_method.h"
 
+#define CLEAR(A) A=decltype(A)()
 map<AST*,unique_ptr<Symbol>> scope_map;
 stack<int> continueStack;
 stack<int> breakStack;
@@ -13,10 +14,10 @@ int next_label=0;
 void astMethodInit()
 {
     scope_map.clear();
-    continueStack={};
-    breakStack={};
-    defaultClauseStack={};
-    functionStack={};
+    CLEAR(continueStack);
+    CLEAR(breakStack);
+    CLEAR(defaultClauseStack);
+    CLEAR(functionStack);
     sp=0;
     this_count=0;
     next_label=0;
